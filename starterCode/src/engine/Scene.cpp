8984bcb3e438732d9069ccc5c5838e64c1849afa
Victor Shen <victorshen98@gmail.com>
2020-02-14 15:11:04 -0500

    Scene children implementation

--- starterCode/src/engine/Scene.cpp
+++ starterCode/src/engine/Scene.cpp
@@ -1,15 +1,39 @@
 #include "Scene.h"
+#include "AnimatedSprite.h"
 #include <string>
 #include <vector>
 #include <fstream>
-// #include <json/value.h>
+#include <iostream>
+#include <json.hpp>
+using json = nlohmann::json;
 
 Scene::Scene(){}
 
 	/* Load scene from a file */
 void Scene::loadScene(string sceneFilePath){
-    // std::ifstream people_file(sceneFilePath, std::ifstream::binary);
-    // people_file >> people;
+    std::ifstream i(sceneFilePath);
+    json j;
+    i >> j;
+    for(int z = 0; z < j["DOC"].size(); z++){
+        std::cout << j["DOC"][z] << endl;
+        DisplayObjectContainer* temp_doc = new DisplayObjectContainer(j["DOC"][z]["name"], j["DOC"][z]["filepath"]);
+        this->addChild(temp_doc);
+    }
+    for(int z = 0; z < j["DO"].size(); z++){
+        std::cout << j["DO"][z] << endl;
+        DisplayObject* temp_do = new DisplayObject(j["DO"][z]["name"], j["DO"][z]["filepath"]);
+        this->addChild(temp_do);
+    }
+    for(int z = 0; z < j["ASprite"].size(); z++){
+        std::cout << j["ASprite"][z] << endl;
+        AnimatedSprite* temp_asprite = new AnimatedSprite(j["ASprite"][z]["name"]);
+        this->addChild(temp_asprite);
+    }
+    for(int z = 0; z < j["Sprite"].size(); z++){
+        std::cout << j["Sprite"][z] << endl;
+        Sprite* temp_sprite = new Sprite(j["Sprite"][z]["name"], j["Sprite"][z]["filepath"]);
+        this->addChild(temp_sprite);
+    }
 }
 
 void Scene::update(set<SDL_Scancode> pressedKeys){
--- starterCode/src/engine/Scene.h
+++ starterCode/src/engine/Scene.h
@@ -22,7 +22,6 @@ public:
 
 
 private:
-
 };
 
 #endif
