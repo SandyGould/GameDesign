067f386398347f379d0a9ff1ff860536932cb13f
mev8vy <mev8vy@virginia.edu>
2020-02-27 23:25:28 -0500

    Place objects, copy displayobjects, objects from file
    Still can't put them in the right place though

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -10,6 +10,45 @@ MyGame::MyGame() : Game(1200, 1000) {
 	instance->addChild(curScene);
 }
 
+
+void MyGame::setupfiles(string path)
+{
+	if(path.compare("./resources/other/") == 0 || path.compare("./resources/scene/") == 0)
+	{
+		return;
+	}
+	DIR *dir;
+	struct dirent *ent;
+	
+	if((dir = opendir(path.c_str()) )!= NULL)
+	{
+		while((ent = readdir(dir) )!= NULL)
+		{
+			
+			if(!ent->d_name || ent->d_name[0] == '.')
+			{
+				continue;
+			}
+			else if(ent->d_type == DT_DIR)
+			{
+				setupfiles(path+ent->d_name+"/");
+			}
+			else
+			{
+				printf("%s\n",ent->d_name);
+				all_sprites.push_back(path+ent->d_name);
+			}
+
+		}
+		closedir(dir);
+	}
+	else
+	{
+		perror("");
+		printf("Could not open...");
+	}
+}
+
 MyGame::MyGame(string sceneToLoad) : Game(1200, 1000){
 	instance = this;
 	curScene = new Scene();
@@ -51,6 +90,16 @@ MyGame::MyGame(string sceneToLoad) : Game(1200, 1000){
 	curScene->addChild(crosshair);
 	printf("Scene added crosshair\n");
 
+
+
+	Coin *coin_proto = new Coin();
+	all_objects.addChild(coin_proto);
+	Player *player_proto = new Player();
+	all_objects.addChild(player_proto);
+	setupfiles("./resources/");
+	printf("Allsprites size = %d\n", all_sprites.size());
+
+	
 	//crosshair_proto = new crosshair();
 
 	//for(int z = 0; z < j["Sprite"].size(); z++){
@@ -58,6 +107,49 @@ MyGame::MyGame(string sceneToLoad) : Game(1200, 1000){
       //  Sprite* temp_sprite = new Sprite(j["Sprite"][z]["name"], j["Sprite"][z]["filepath"]);
         //this->addChild(temp_sprite);
     //}
+}
+
+//change this to containers
+void MyGame::copyDisplayObjectContainer(DisplayObjectContainer *newobj, DisplayObjectContainer *oldobj) {
+	newobj->position = oldobj->position;
+	newobj->width = oldobj->width;
+	newobj-> height = oldobj->height;
+	newobj->pivot = oldobj->pivot;
+	newobj->scaleX = oldobj->scaleX;
+	newobj->scaleY = oldobj->scaleY;
+	newobj->rotation = oldobj->rotation; // in radians
+	newobj->facingRight = oldobj->facingRight;
+	newobj->hasCollision = oldobj->hasCollision;
+	copy(oldobj->children.begin(),oldobj->children.end(),back_inserter(newobj->children));
+	copy(oldobj->collisionList.begin(),oldobj->collisionList.end(),back_inserter(newobj->collisionList));
+	newobj->id = oldobj->id;
+	newobj->imgPath = oldobj->imgPath;
+	newobj->loadTexture(newobj->imgPath);
+	
+
+}
+
+
+void MyGame::copyDisplayObject(DisplayObject *newobj, DisplayObject *oldobj) {
+	newobj->position = oldobj->position;
+	newobj->width = oldobj->width;
+	newobj-> height = oldobj->height;
+	newobj->pivot = oldobj->pivot;
+	newobj->scaleX = oldobj->scaleX;
+	newobj->scaleY = oldobj->scaleY;
+	newobj->rotation = oldobj->rotation; // in radians
+	newobj->facingRight = oldobj->facingRight;
+	newobj->hasCollision = oldobj->hasCollision;
+	copy(static_cast<DisplayObjectContainer*>(oldobj)->children.begin(),(static_cast<DisplayObjectContainer*>(oldobj)->children.end()),back_inserter((static_cast<DisplayObjectContainer*>(newobj)->children)));
+	copy(static_cast<DisplayObjectContainer*>(oldobj)->collisionList.begin(),static_cast<DisplayObjectContainer*>(oldobj)->collisionList.end(),back_inserter(static_cast<DisplayObjectContainer*>(newobj)->collisionList));
+	newobj->id = oldobj->id;
+	newobj->imgPath = oldobj->imgPath;
+	newobj->loadTexture(newobj->imgPath);
+	///Coin *coin_proto = new Coin();
+	//all_objects.addChild(coin_proto);
+	//Player *player_proto = new Player();
+	//all_objects.addChild(player_proto);
+
 }
 
 MyGame::~MyGame() {
@@ -121,40 +213,73 @@ void MyGame::update(std::set<SDL_Scancode> pressedKeys) {
 		camera->zoomOut(1.1);
 	}
 	if (pressedKeys.find(SDL_SCANCODE_TAB) != pressedKeys.end()) {
-		if(hasChild == false)
+		if(grabbedObj == false)
 		{
-			Coin * childcoin = new Coin();
-			crosshair->addChild(childcoin);
-			hasChild = true;		
+
+			if(obj_ind < all_sprites.size()-1)
+			{
+				printf("obj_ind == %d\n",obj_ind);
+				if(obj_ind > 0)
+				{
+					DisplayObject * myobjc = crosshair->getChild(0);
+					crosshair->removeImmediateChild(myobjc);
+				}
+				obj_ind++;
+				Sprite * newobj = new Sprite("newobject",all_sprites[obj_ind]);
+				//DisplayObject * childobj = all_objects.getChild(obj_ind);
+				//DisplayObject * newobj = new DisplayObject();
+				//copyDisplayObject(newobj,childobj);
+				crosshair->addChild(newobj);
+				hasChild = true;
+				
+				
+
+			} 
+			else
+			{
+				obj_ind = 0;
+				DisplayObject * myobjc = crosshair->getChild(0);
+					crosshair->removeImmediateChild(myobjc);
+				hasChild = false;
+			}
+		//	Coin * childcoin = new Coin();
+		//	crosshair->addChild(childcoin);
+		//	hasChild = true;		
 		}
-		else
-		{
-			DisplayObject * mycoin = crosshair->getChild(0);
-			crosshair->removeImmediateChild(mycoin);
-			hasChild = false;
-		}
+		//else
+		//{
+		//	DisplayObject * mycoin = crosshair->getChild(0);
+		//	crosshair->removeImmediateChild(mycoin);
+		//	hasChild = false;
+		//}
 	}
 	if (pressedKeys.find(SDL_SCANCODE_RETURN) != pressedKeys.end()) {
-		if(hasChild == true)
+		if(hasChild == true || grabbedObj == true)
 		{
-			DisplayObject* mycoin = crosshair->getChild(0);
-			Coin * newcoin = new Coin();
-			bool visible = true;
-			newcoin->position = mycoin->position;
-			newcoin->width = mycoin->width;
-			newcoin-> height = mycoin->height;
-			newcoin->pivot = mycoin->pivot;
-			newcoin->scaleX = mycoin->scaleX;
-			newcoin->scaleY = mycoin->scaleY;
-			newcoin->rotation = mycoin->rotation; // in radians
+			DisplayObject * myobj = crosshair->getChild(0);
+			DisplayObject* newobj = new DisplayObjectContainer();
+			copyDisplayObject(newobj,myobj);
+			curScene->addChild(newobj);
+			crosshair->removeImmediateChild(myobj);
+			hasChild = false;
+			obj_ind = 0;
+			grabbedObj = false;
+			//bool visible = true;
+			//newcoin->position = mycoin->position;
+			//newcoin->width = mycoin->width;
+			//newcoin-> height = mycoin->height;
+			//newcoin->pivot = mycoin->pivot;
+			//newcoin->scaleX = mycoin->scaleX;
+			//newcoin->scaleY = mycoin->scaleY;
+			//newcoin->rotation = mycoin->rotation; // in radians
 			
 
-			bool hasCollision = false;
-			crosshair->removeImmediateChild(mycoin);
-			printf("About to add child to scene\n");
-			curScene->addChild(newcoin);
-			printf("Child added?\n");
-			hasChild = false;	
+			//bool hasCollision = false;
+			//crosshair->removeImmediateChild(mycoin);
+			//printf("About to add child to scene\n");
+			//curScene->addChild(newcoin);
+			//printf("Child added?\n");
+			//hasChild = false;	
 		}
 		
 	}
--- src/main/MyGame.h
+++ src/main/MyGame.h
@@ -13,6 +13,11 @@
 #include "../engine/events/PickedUpEvent.h"
 #include "../engine/Scene.h"
 #include <vector>
+#include <iterator>
+#include <algorithm>
+#include <stdio.h>
+#include <dirent.h>
+//#include <filesystem>
 
 class MyGame : public Game {
 
@@ -23,19 +28,22 @@ public:
 
 	void update(std::set<SDL_Scancode> pressedKeys) override;
 	void draw(AffineTransform& at) override;
-
+	void copyDisplayObjectContainer(DisplayObjectContainer *newobj, DisplayObjectContainer *oldobj);
+	void copyDisplayObject(DisplayObject* newobj, DisplayObject *oldobj);
+	void setupfiles(string path);
 private:
 	
 	Camera* camera;
 	bool inZone = false;
 	bool hasChild = false;
-	
-	//DisplayObjectContainer all_objects;
-
+	bool grabbedObj = false;
+	int obj_ind = 0;
+	DisplayObjectContainer all_objects;
+	vector<string> all_sprites;
 	Crosshair* crosshair;
 	Scene* curScene;
 
-	DisplayObjectContainer* allSprites;
+	//DisplayObjectContainer* allSprites;
 
 	QuestManager* questManager;
 
