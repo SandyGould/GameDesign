92d21e75b497f7a5f58a71d0e38b8d45b0f7523b
Jason Tufano <jt2ef@virginia.edu>
2020-03-05 15:03:30 -0500

    Made main slightly more robust.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,6 +2,7 @@
 #include "Editor.h"
 
 #include <string>
+#include <iostream>
 
 // SDL tries to redefine main? Need to investigate later
 #undef main
@@ -9,12 +10,26 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	if (argc == 2) {
-		string scene(argv[1]);
-
-		// Making this not a pointer causes SDL assert errors when quitting?
-		Editor* editor = new Editor(scene);
-		editor->start();
+	if (argc > 3) {
+		cout << "Unrecognized arguments, launching game." << endl;
+		Rebound* rebound = new Rebound();
+		rebound->start();
+	} else if (argc > 1){
+		string edit(argv[1]);
+		if (edit != "editor"){
+			cout << "Unrecognized arguments, launching game." << endl;
+			Rebound* rebound = new Rebound();
+			rebound->start();
+		} else{
+			Editor* editor;
+			if (argc == 3){
+				string scene(argv[2]);
+				editor = new Editor(scene);
+			} else{
+				editor = new Editor();
+			}
+			editor->start();
+		}
 	} else {
 		Rebound* rebound = new Rebound();
 		rebound->start();
