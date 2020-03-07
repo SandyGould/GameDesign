a758e1a48eda748f03a9258ffc3f1d981e34f900
Winston Liu <wliu1402@gmail.com>
2020-03-07 18:03:57 -0500

    Initial clang-format config
    Reformat DisplayObject.cpp ahead of combining DO and DOC

--- src/engine/DisplayObject.cpp
+++ src/engine/DisplayObject.cpp
@@ -1,4 +1,5 @@
 #include "DisplayObject.h"
+
 #include "Game.h"
 
 #include <algorithm>
@@ -8,165 +9,171 @@
 constexpr auto PI = 3.14159265;
 
 DisplayObject::DisplayObject() {
-	image = NULL;
-	texture = NULL;
-	curTexture = NULL;
+    image = NULL;
+    texture = NULL;
+    curTexture = NULL;
 }
 
 DisplayObject::DisplayObject(std::string id, std::string filepath) {
-	this->id = id;
-	this->imgPath = filepath;
+    this->id = id;
+    this->imgPath = filepath;
 
-	loadTexture(filepath, Game::renderer);
+    loadTexture(filepath, Game::renderer);
 }
 
 DisplayObject::DisplayObject(std::string id, std::string filepath, SDL_Renderer* r) {
-	this->id = id;
-	this->imgPath = filepath;
+    this->id = id;
+    this->imgPath = filepath;
 
-	loadTexture(filepath, r);
+    loadTexture(filepath, r);
 }
 
 DisplayObject::DisplayObject(std::string id, int red, int green, int blue) {
-	isRGB = true;
-	this->id = id;
+    isRGB = true;
+    this->id = id;
 
-	this->red = red;
-	this->blue = blue;
-	this->green = green;
+    this->red = red;
+    this->blue = blue;
+    this->green = green;
 
-	this->loadRGBTexture(red, green, blue);
+    this->loadRGBTexture(red, green, blue);
 }
 
 DisplayObject::DisplayObject(const DisplayObject& other) {
-	this->position = other.position;
-	this->width = other.width;
-	this->height = other.height;
-	this->pivot = other.pivot;
-	this->scaleX = other.scaleX;
-	this->scaleY = other.scaleY;
-	this->rotation = other.rotation; // in radians
-	this->facingRight = other.facingRight;
-	this->hasCollision = other.hasCollision;
-	// copy(static_cast<DisplayObjectContainer*>(other)->children.begin(), (static_cast<DisplayObjectContainer*>(other)->children.end()), back_inserter((static_cast<DisplayObjectContainer*>(this)->children)));
-	// copy(static_cast<DisplayObjectContainer*>(other)->collisionList.begin(), static_cast<DisplayObjectContainer*>(other)->collisionList.end(), back_inserter(static_cast<DisplayObjectContainer*>(this)->collisionList));
-	this->id = other.id + "_copy";
-	this->imgPath = other.imgPath;
-	this->loadTexture(this->imgPath, Game::renderer);
+    this->position = other.position;
+    this->width = other.width;
+    this->height = other.height;
+    this->pivot = other.pivot;
+    this->scaleX = other.scaleX;
+    this->scaleY = other.scaleY;
+    this->rotation = other.rotation; // in radians
+    this->facingRight = other.facingRight;
+    this->hasCollision = other.hasCollision;
+    // copy(static_cast<DisplayObjectContainer*>(other)->children.begin(),
+    // (static_cast<DisplayObjectContainer*>(other)->children.end()),
+    // back_inserter((static_cast<DisplayObjectContainer*>(this)->children)));
+    // copy(static_cast<DisplayObjectContainer*>(other)->collisionList.begin(),
+    // static_cast<DisplayObjectContainer*>(other)->collisionList.end(),
+    // back_inserter(static_cast<DisplayObjectContainer*>(this)->collisionList));
+    this->id = other.id + "_copy";
+    this->imgPath = other.imgPath;
+    this->loadTexture(this->imgPath, Game::renderer);
 }
 
 DisplayObject::~DisplayObject() {
-	//TODO: Get this freeing working
-	if(image != NULL) SDL_FreeSurface(image);
-	if(texture != NULL) SDL_DestroyTexture(texture);
+    // TODO: Get this freeing working
+    if (image != NULL) {
+        SDL_FreeSurface(image);
+    }
+
+    if (texture != NULL) {
+        SDL_DestroyTexture(texture);
+    }
 }
 
 void DisplayObject::loadTexture(std::string filepath, SDL_Renderer* r) {
-	image = IMG_Load(filepath.c_str());
-	texture = SDL_CreateTextureFromSurface(r, image);
-	setTexture(texture);
+    image = IMG_Load(filepath.c_str());
+    texture = SDL_CreateTextureFromSurface(r, image);
+    setTexture(texture);
 }
 
 void DisplayObject::loadRGBTexture(int red, int green, int blue) {
-	image = SDL_CreateRGBSurface(0, 100, 100, 32, 0, 0, 0, 0x000000ff);
-	SDL_FillRect(image, NULL, SDL_MapRGB(image->format, red, green, blue));
-	texture = SDL_CreateTextureFromSurface(Game::renderer, image);
-	SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
-	setTexture(texture);
+    image = SDL_CreateRGBSurface(0, 100, 100, 32, 0, 0, 0, 0x000000ff);
+    SDL_FillRect(image, NULL, SDL_MapRGB(image->format, red, green, blue));
+    texture = SDL_CreateTextureFromSurface(Game::renderer, image);
+    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+    setTexture(texture);
 }
 
-SDL_Point DisplayObject::getGlobalPosition(AffineTransform& at){
-	DisplayObject* temp = this->parent;
-	std::vector<DisplayObject*> parentList;
-	// && temp->type != "Scene"
-	while (temp != NULL){
-		parentList.push_back(temp);
-		temp = temp->parent;
-	}
-	for (auto i = parentList.rbegin(); i != parentList.rend(); ++i){
-		(*i)->applyTransformations(at);
-		at.translate((*i)->pivot.x, (*i)->pivot.y);
-	}
-	applyTransformations(at);
-	SDL_Point origin = at.transformPoint(0, 0);
-	reverseTransformations(at);
-	for (auto i = parentList.begin(); i != parentList.end(); ++i){
-		at.translate(-(*i)->pivot.x, -(*i)->pivot.y);
-		(*i)->reverseTransformations(at);
-	}
-	return origin;
+SDL_Point DisplayObject::getGlobalPosition(AffineTransform& at) {
+    DisplayObject* temp = this->parent;
+    std::vector<DisplayObject*> parentList;
+    // && temp->type != "Scene"
+    while (temp != NULL) {
+        parentList.push_back(temp);
+        temp = temp->parent;
+    }
+    for (auto i = parentList.rbegin(); i != parentList.rend(); ++i) {
+        (*i)->applyTransformations(at);
+        at.translate((*i)->pivot.x, (*i)->pivot.y);
+    }
+    applyTransformations(at);
+    SDL_Point origin = at.transformPoint(0, 0);
+    reverseTransformations(at);
+    for (auto i = parentList.begin(); i != parentList.end(); ++i) {
+        at.translate(-(*i)->pivot.x, -(*i)->pivot.y);
+        (*i)->reverseTransformations(at);
+    }
+    return origin;
 }
 
 void DisplayObject::setTexture(SDL_Texture* t) {
-	this->curTexture = t;
+    this->curTexture = t;
 }
 
-void DisplayObject::update(std::unordered_set<SDL_Scancode> pressedKeys) {
-
-}
+void DisplayObject::update(std::unordered_set<SDL_Scancode> pressedKeys) {}
 
 void DisplayObject::draw(AffineTransform& at) {
-	DisplayObject::draw(at, Game::renderer);
+    DisplayObject::draw(at, Game::renderer);
 }
 
 void DisplayObject::draw(AffineTransform& at, SDL_Renderer* r) {
-	applyTransformations(at);
+    applyTransformations(at);
 
-	if(curTexture != NULL && visible) {
-		SDL_Point origin = at.transformPoint(0, 0);
-		SDL_Point upperRight = at.transformPoint(width, 0);
-		SDL_Point lowerRight = at.transformPoint(width, height);
-		SDL_Point corner = {0, 0};
+    if (curTexture != NULL && visible) {
+        SDL_Point origin = at.transformPoint(0, 0);
+        SDL_Point upperRight = at.transformPoint(width, 0);
+        SDL_Point lowerRight = at.transformPoint(width, height);
+        SDL_Point corner = {0, 0};
 
-		// std::cout << position.x << endl;
-		int w = (int)distance(origin, upperRight);
-		int h = (int)distance(upperRight, lowerRight);
+        // std::cout << position.x << endl;
+        int w = (int)distance(origin, upperRight);
+        int h = (int)distance(upperRight, lowerRight);
 
-		this->dstrect = { origin.x, origin.y, w, h };
+        this->dstrect = {origin.x, origin.y, w, h};
 
-		SDL_RendererFlip flip;
-		if (facingRight) {
-			flip = SDL_FLIP_NONE;
-		}
-		else {
-			flip = SDL_FLIP_HORIZONTAL;
-		}
+        SDL_RendererFlip flip;
+        if (facingRight) {
+            flip = SDL_FLIP_NONE;
+        } else {
+            flip = SDL_FLIP_HORIZONTAL;
+        }
 
-		SDL_SetTextureAlphaMod(curTexture, alpha);
-		SDL_RenderCopyEx(r, curTexture, NULL, &dstrect, calculateRotation(origin, upperRight), &corner, flip);
-	}
+        SDL_SetTextureAlphaMod(curTexture, alpha);
+        SDL_RenderCopyEx(r, curTexture, NULL, &dstrect, calculateRotation(origin, upperRight), &corner, flip);
+    }
 
-	reverseTransformations(at);
+    reverseTransformations(at);
 }
 
 void DisplayObject::applyTransformations(AffineTransform& at) {
-	at.translate(position.x, position.y);
-	at.rotate(rotation);
-	at.scale(scaleX, scaleY);
-	at.translate(-pivot.x, -pivot.y);
+    at.translate(position.x, position.y);
+    at.rotate(rotation);
+    at.scale(scaleX, scaleY);
+    at.translate(-pivot.x, -pivot.y);
 }
 
 void DisplayObject::reverseTransformations(AffineTransform& at) {
-	at.translate(pivot.x, pivot.y);
-	at.scale(1.0/scaleX, 1.0/scaleY);
-	at.rotate(-rotation);
-	at.translate(-position.x, -position.y);
+    at.translate(pivot.x, pivot.y);
+    at.scale(1.0 / scaleX, 1.0 / scaleY);
+    at.rotate(-rotation);
+    at.translate(-position.x, -position.y);
 }
 
 int DisplayObject::getWidth() {
-	return this->image->w;
+    return this->image->w;
 }
 
 int DisplayObject::getHeight() {
-	return this->image->h;
+    return this->image->h;
 }
 
 double DisplayObject::distance(SDL_Point& p1, SDL_Point& p2) {
-	return std::sqrt(((p2.y - p1.y) * (p2.y - p1.y)) + ((p2.x - p1.x) * (p2.x - p1.x)));
+    return std::sqrt(((p2.y - p1.y) * (p2.y - p1.y)) + ((p2.x - p1.x) * (p2.x - p1.x)));
 }
 
 double DisplayObject::calculateRotation(SDL_Point& origin, SDL_Point& p) {
-	int y = p.y - origin.y;
-	int x = p.x - origin.x;
-	return std::atan2(y, x) * 180 / PI;
+    int y = p.y - origin.y;
+    int x = p.x - origin.x;
+    return std::atan2(y, x) * 180 / PI;
 }
