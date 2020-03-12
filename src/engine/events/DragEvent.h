2bc50b3981a5f76a2999dfa6cfe51bf0528ac8cf
Winston Liu <Wliu1402@gmail.com>
2020-03-11 23:24:09 -0400

    Format

--- src/engine/events/DragEvent.h
+++ src/engine/events/DragEvent.h
@@ -5,14 +5,14 @@
 
 class DragEvent : public Event {
 public:
-	inline static const std::string DRAG_EVENT = "drag_event";
+    inline static const std::string DRAG_EVENT = "drag_event";
 
-	DragEvent(int x, int y, int xrel, int yrel, int window, SDL_Keymod modifiers);
+    DragEvent(int x, int y, int xrel, int yrel, int window, SDL_Keymod modifiers);
 
-	int x;
-	int y;
-	int xrel;
-	int yrel;
-	int wID;
+    int x;
+    int y;
+    int xrel;
+    int yrel;
+    int wID;
     SDL_Keymod modifiers;
 };
