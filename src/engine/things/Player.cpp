36935ce73b596895850f6121119eb52939f7fef3
Alex Robiolio <amr5tk@virginia.edu>
2020-04-02 21:23:17 -0500

    Updated player to have health and stamina

--- src/engine/things/Player.cpp
+++ src/engine/things/Player.cpp
@@ -1,11 +1,24 @@
 #include "Player.h"
 
-Player::Player() : AnimatedSprite("player", "./resources/assets/Spritesheets/Girl/Girl.png", "./resources/assets/Spritesheets/Girl.xml"){//Sprite("player", "./resources/character/Idle_1.png") {
+Player::Player() : AnimatedSprite("player", "./resources/assets/Spritesheets/Girl/Girl.png", "./resources/assets/Spritesheets/Girl.xml") {
     type = "player";
     hasCollision = true;
 }
 
-
+void Player::changeHealth(int amount) {
+    if (health + amount < 100) {
+        health += amount;
+    } else {
+        health = 100;
+    }
+}
+void Player::changeStamina(int amount) {
+    if (stamina + amount < 1000) {
+        stamina += amount;
+    } else {
+        stamina = 1000;
+    }
+}
 
 void Player::update(std::unordered_set<SDL_Scancode> pressedKeys, jState joystickState, std::unordered_set<Uint8> pressedButtons) {
     AnimatedSprite::update(pressedKeys, joystickState, pressedButtons);
--- src/engine/things/Player.h
+++ src/engine/things/Player.h
@@ -10,10 +10,15 @@ public:
 
 	Player();
 
+	void changeHealth(int amount);
+	void changeStamina(int amount);
+
 	void update(std::unordered_set<SDL_Scancode> pressedKeys, jState joystickState, std::unordered_set<Uint8> pressedButtons) override;
 	void draw(AffineTransform& at) override;
 
 private:
+	int health = 100;
+	int stamina = 1000;
 
 };
 
