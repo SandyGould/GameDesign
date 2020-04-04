e18ad29edf4577d283cc24b5c48fb8b66d4c9fbb
Winston Liu <Wliu1402@gmail.com>
2020-04-03 20:48:41 -0400

    Collision demo is functionally complete 🎉

--- src/main/demos/CollisionDemo.cpp
+++ src/main/demos/CollisionDemo.cpp
@@ -22,91 +22,137 @@ CollisionDemo::CollisionDemo() : Game(1200, 800) {
 
     camera->addChild(this->scene);
 
-    this->moon = new Coin();
-    this->moon->position = {this->windowWidth / 2, this->windowHeight / 2};
-    this->moon->width = this->moon->height = 100;
-    this->moon->pivot = {50, 50};
-    this->scene->getChild(0)->addChild(this->moon);
+    this->parentObj = new DisplayObject("parent", "./resources/assets/Display_Objects/Moon.png");
+    this->parentObj->type = "parent";
+    this->parentObj->position = {this->windowWidth / 2 - 100, this->windowHeight / 2 - 100};
+    this->parentObj->width = this->parentObj->height = 200;
+    this->parentObj->pivot = {100, 100};
+    this->scene->getChild(0)->addChild(this->parentObj);
 
-    this->coin = new Coin();
-    this->coin->position = {this->windowWidth / 2 + 100, this->windowHeight / 2 + 100};
-    this->coin->width = this->coin->height = 30;
-    this->coin->pivot = {15, 15};
-    this->scene->getChild(0)->addChild(this->coin);
+    this->child1 = new Coin();
+    this->child1->position = {200, 0};
+    this->child1->width = this->child1->height = 100;
+    this->child1->pivot = {50, 50};
+    this->parentObj->addChild(this->child1);
+
+    this->child2 = new Coin();
+    this->child2->position = {200, 200};
+    this->child2->width = this->child2->height = 30;
+    this->child2->pivot = {15, 15};
+    this->parentObj->addChild(this->child2);
 
     this->collisionSystem->watchForCollisions("coin", "coin");
 }
 
 CollisionDemo::~CollisionDemo() {
+    delete camera;
+    delete scene;
+    delete parentObj;
+    delete child1;
+    delete child2;
 }
 
 void CollisionDemo::update(std::unordered_set<SDL_Scancode> pressedKeys, jState joystickState, std::unordered_set<Uint8> pressedButtons) {
+    if (pressedKeys.find(SDL_SCANCODE_W) != pressedKeys.end()) {
+        this->parentObj->position.y -= 5;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_A) != pressedKeys.end()) {
+        this->parentObj->position.x -= 5;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_S) != pressedKeys.end()) {
+        this->parentObj->position.y += 5;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_D) != pressedKeys.end()) {
+        this->parentObj->position.x += 5;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_E) != pressedKeys.end()) {
+        this->parentObj->rotation -= 0.0314159265;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_R) != pressedKeys.end()) {
+        this->parentObj->rotation += 0.0314159265;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_F) != pressedKeys.end()) {
+        this->parentObj->scaleX -= 0.1;
+        this->parentObj->scaleY -= 0.1;
+    }
+
+    if (pressedKeys.find(SDL_SCANCODE_G) != pressedKeys.end()) {
+        this->parentObj->scaleX += 0.1;
+        this->parentObj->scaleY += 0.1;
+    }
+
     if (pressedKeys.find(SDL_SCANCODE_I) != pressedKeys.end()) {
-        this->moon->position.y -= 5;
+        this->child1->position.y -= 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_J) != pressedKeys.end()) {
-        this->moon->position.x -= 5;
+        this->child1->position.x -= 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_K) != pressedKeys.end()) {
-        this->moon->position.y += 5;
+        this->child1->position.y += 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_L) != pressedKeys.end()) {
-        this->moon->position.x += 5;
+        this->child1->position.x += 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_O) != pressedKeys.end()) {
-        this->moon->rotation -= 0.0314159265;
+        this->child1->rotation -= 0.0314159265;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_P) != pressedKeys.end()) {
-        this->moon->rotation += 0.0314159265;
+        this->child1->rotation += 0.0314159265;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_N) != pressedKeys.end()) {
-        this->moon->scaleX -= 0.1;
-        this->moon->scaleY -= 0.1;
+        this->child1->scaleX -= 0.1;
+        this->child1->scaleY -= 0.1;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_M) != pressedKeys.end()) {
-        this->moon->scaleX += 0.1;
-        this->moon->scaleY += 0.1;
+        this->child1->scaleX += 0.1;
+        this->child1->scaleY += 0.1;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_UP) != pressedKeys.end()) {
-        this->coin->position.y -= 5;
+        this->child2->position.y -= 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_LEFT) != pressedKeys.end()) {
-        this->coin->position.x -= 5;
+        this->child2->position.x -= 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_DOWN) != pressedKeys.end()) {
-        this->coin->position.y += 5;
+        this->child2->position.y += 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_RIGHT) != pressedKeys.end()) {
-        this->coin->position.x += 5;
+        this->child2->position.x += 5;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_X) != pressedKeys.end()) {
-        this->coin->rotation -= 0.0314159265;
+        this->child2->rotation -= 0.0314159265;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_C) != pressedKeys.end()) {
-        this->coin->rotation += 0.0314159265;
+        this->child2->rotation += 0.0314159265;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_V) != pressedKeys.end()) {
-        this->coin->scaleX -= 0.1;
-        this->coin->scaleY -= 0.1;
+        this->child2->scaleX -= 0.1;
+        this->child2->scaleY -= 0.1;
     }
 
     if (pressedKeys.find(SDL_SCANCODE_B) != pressedKeys.end()) {
-        this->coin->scaleX += 0.1;
-        this->coin->scaleY += 0.1;
+        this->child2->scaleX += 0.1;
+        this->child2->scaleY += 0.1;
     }
 
     // Check for collisions after we finalize ideal positions
@@ -120,6 +166,7 @@ void CollisionDemo::draw(AffineTransform& at) {
 
 void CollisionDemo::draw_post() {
     SDL_SetRenderDrawColor(Game::renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-    this->coin->drawHitbox();
-    this->moon->drawHitbox();
+    this->parentObj->drawHitbox();
+    this->child1->drawHitbox();
+    this->child2->drawHitbox();
 }
