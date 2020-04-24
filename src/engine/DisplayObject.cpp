6a6dfcad38b7f5c07ccda1e62a64f3218b11a342
Sarah Benson <srb5uw@virginia.edu>
2020-04-24 13:41:00 -0400

    fixed DO handleEvent

--- src/engine/DisplayObject.cpp
+++ src/engine/DisplayObject.cpp
@@ -410,8 +410,8 @@ void DisplayObject::propogateEvent(Event* e, std::shared_ptr<DisplayObject> root
         for (auto child : root->children) {
             propogateEvent(e, child);
         }
-        auto out_transition = std::make_shared<Tween>("out_transition", root);
-		out_transition->animate(TweenableParams::ALPHA, 255, 0, 200, TweenParam::EASE_IN);
+        auto out_transition = std::make_shared<Tween>(root->id + "_out_transition", root);
+		out_transition->animate(TweenableParams::ALPHA, 255, 0, 100, TweenParam::EASE_IN);
 		TweenJuggler::getInstance().add(out_transition);
     }
     if (e->getType() == NewSceneEvent::FADE_IN_EVENT){
@@ -419,42 +419,29 @@ void DisplayObject::propogateEvent(Event* e, std::shared_ptr<DisplayObject> root
         for (auto child : root->children) {
             propogateEvent(e, child);
         }
-        auto in_transition = std::make_shared<Tween>("in_transition", root);
-		in_transition->animate(TweenableParams::ALPHA, 0, 255, 200, TweenParam::EASE_IN);
+        auto in_transition = std::make_shared<Tween>(root->id + "_in_transition", root);
+		in_transition->animate(TweenableParams::ALPHA, 0, 255, 100, TweenParam::EASE_IN);
 		TweenJuggler::getInstance().add(in_transition);
 	}
 }
 
 void DisplayObject::handleEvent(Event* e){
-    // once tween is done, delete scene
-    if (e->getType() == TweenEvent::TWEEN_COMPLETE_EVENT) {
-        if (((TweenEvent*) e)->getTween()->getID() == "out_transition") {
-            if (this->type == "Scene") {
-                EventDispatcher::getInstance().removeEventListener(this, TweenEvent::TWEEN_COMPLETE_EVENT);
-                for (auto child : children) {
-                    this->removeImmediateChild(child);
-                }
-
-                this->removeThis();
-            }
-        }
-    }
     // scale out event
     if (e->getType() == NewSceneEvent::SCALE_OUT_EVENT) {
         EventDispatcher::getInstance().removeEventListener(this, NewSceneEvent::SCALE_OUT_EVENT);
         double curScaleX = this->scaleX;
         double curScaleY = this->scaleY;
         auto out_transition = std::make_shared<Tween>("out_transition", shared_from_this());
-		out_transition->animate(TweenableParams::SCALE_X, curScaleX, 0, 200, TweenParam::EASE_IN);
-		out_transition->animate(TweenableParams::SCALE_Y, curScaleY, 0, 200, TweenParam::EASE_IN);
+		out_transition->animate(TweenableParams::SCALE_X, curScaleX, 0, 100, TweenParam::EASE_IN);
+		out_transition->animate(TweenableParams::SCALE_Y, curScaleY, 0, 100, TweenParam::EASE_IN);
 		TweenJuggler::getInstance().add(out_transition);
     }
     // scale in event
     if (e->getType() == NewSceneEvent::SCALE_IN_EVENT) {
         EventDispatcher::getInstance().removeEventListener(this, NewSceneEvent::SCALE_IN_EVENT);
         auto in_transition = std::make_shared<Tween>("in_transition", shared_from_this());
-		in_transition->animate(TweenableParams::SCALE_X, 0, 1, 200, TweenParam::EASE_IN);
-		in_transition->animate(TweenableParams::SCALE_Y, 0, 1, 200, TweenParam::EASE_IN);
+		in_transition->animate(TweenableParams::SCALE_X, 0, 1, 100, TweenParam::EASE_IN);
+		in_transition->animate(TweenableParams::SCALE_Y, 0, 1, 100, TweenParam::EASE_IN);
 		TweenJuggler::getInstance().add(in_transition);
     }
     // scale in event
