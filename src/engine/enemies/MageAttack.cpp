08e2cf00553fc71e62cd02524b8d05f29e747888
Winston Liu <Wliu1402@gmail.com>
2020-04-30 16:50:45 -0400

    Change this back to 6 lol

--- src/engine/enemies/MageAttack.cpp
+++ src/engine/enemies/MageAttack.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 MageAttack::MageAttack() : Projectile("mage_attack", "./resources/assets/Animated_Sprites/Enemies/Mage/mageAttack.png", 12){
-    this->width = 36;
-    this->height = 36;
+    this->width = 6;
+    this->height = 6;
     this->type = "mage_attack";
     this->magic = true;
     this->saveType = this->type;
