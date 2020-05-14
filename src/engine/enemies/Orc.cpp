38d99fd045e753718a9d8c62b37b6acfc7b5d857
Winston Liu <Wliu1402@gmail.com>
2020-05-14 13:59:08 -0400

    Orcs do damage now

--- src/engine/enemies/Orc.cpp
+++ src/engine/enemies/Orc.cpp
@@ -4,5 +4,6 @@ static int orc_count = 1;
 
 Orc::Orc(std::shared_ptr<Player> player) : Knight(player, "./resources/assets/Animated_Sprites/Enemies/enemies.png", "./resources/assets/Animated_Sprites/Enemies/enemies.xml", "OrcIdle"){
     this->id = "orc" + std::to_string(orc_count);
+    this->type = "orc";
     orc_count++;
 }
